7d5a73c6486159c7b4b142d002da6be3a062ec6e
thephisics101 <thephisics101@gmail.com>
2020-03-08 21:01:30 +0200

    Fix typos

--- src/scripts/MemGame.ts
+++ src/scripts/MemGame.ts
@@ -2,7 +2,7 @@ class MemoryGame {
   selected = -1;
   revealed = [];
   entries = [];
-  size = {};
+  size = {width: 0, height: 0};
   won = false;
 
   static RESULT_STATE = {
@@ -17,14 +17,14 @@ class MemoryGame {
     if (typeof size.width !== "number" || typeof size.height !== "number") throw new Error("Properties 'width' and 'height' mst be numbers");
     if (size.width <= 0 || size.height <= 0) throw  new Error("Width and height must be positive!");
     if ((size.width * size.height) % 2 === 1) throw new Error("Width and height can't both be odd!");
-    if (size.x * size.y / 2 > PAIRS.length) throw new Error("not enough pairs for this grid size");
+    if (size.width * size.height / 2 > PAIRS.length) throw new Error("Not enough pairs for this grid size");
     this.size = size;
     this.entries = PAIRS.shuffle().slice(0, size.width*size.height/2).flat().shuffle();
     this.revealed = new Array(this.entries.length).fill(false);
   }
 
   select(index) {
-    if (index > this.entries.length) throw new Error("index should not be larger than x * y");
+    if (index > this.size.width * this.size.height) throw new Error("index should not be larger than x * y");
     if (this.revealed[index]) return MemoryGame.RESULT_STATE.CANCEL;
     if (this.selected > -1) return this._match(index);
     this.revealed[index] = true;
