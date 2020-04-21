76f527aa35b8c76a0f5b384660e09e91302f204c
thephisics101 <thephisics101@gmail.com>
2020-04-21 14:23:55 +0300

    Add API test 2

--- src/scripts/ScoreAPI.ts
+++ src/scripts/ScoreAPI.ts
@@ -1,6 +1,9 @@
 const ScoreAPI = {
   getScore: () => {},
   postScore: () => {
-    fetch("/API/score", {method: "POST", body: '{name: "test", score: 0, size: 1}'}).catch(e => {throw e});
+    fetch("/API/score", {
+      method: "POST", body: '{name: "test", score: 0, size: 1}',
+      headers: {"content-type": "application/json"}
+    }).catch(e => {throw e});
   }
 };
