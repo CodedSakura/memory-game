d7237789dd724e07913a49c39297406270d1b58e
thephisics101 <thephisics101@gmail.com>
2020-04-21 13:31:22 +0300

    Add basic API to Flask

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="./web", static_folder="./static")
-
 app.config["DEBUG"] = True
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="the username from the 'Databases' tab",
+    password="the password you set on the 'Databases' tab",
+    hostname="the database host address from the 'Databases' tab",
+    databasename="the database name from the 'Databases' tab",
+)
+app.config["SQLALCHEMY_POLL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class ScoreObj(db.Model):
+    __tablename__ = "score"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(4096))
+    score = db.Column(db.Integer)
+    size = db.Column(db.Integer)
+
+    static def from_req(req):
+        pass
+
 
 @app.route("/")
 def index():
@@ -10,4 +33,9 @@ def index():
 
 @app.route("/API/score", methods=["GET", "POST"])
 def score():
-    pass
+    if request.method == "GET":
+        return "{}"
+    if request.method == "POST":
+    score_obj = ScoreObj.from_req(request)
+    db.session.add(score_obj)
+    db.session.commit()
